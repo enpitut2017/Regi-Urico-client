2848df9cf2b23b4df85b94cb1bbbb17f333e8937
urushiyama <aswif10flis1ntkb@gmail.com>
2017-11-15 14:02:35 +0900

    Add GET EventItemsList action

--- src/App.js
+++ src/App.js
@@ -2,23 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import EventItemsList from './EventItemsList.js';
-import CheckoutButton from './CheckoutButton.js';
+import axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = this.getInitialState();
+    this.handleEventIdChange = this.handleEventIdChange.bind(this);
   }
 
   getInitialState() {
     return {
-      //items: []
-      items: [
-        {"price":1400,"item_id":1,"name":"GENSOUM@STER","count":10},
-        {"price":600,"item_id":2,"name":"東方魔烈槍","count":15}
-      ]
+      event_id: 0,
+      items: [],
     };
   }
+  
+  handleEventIdChange(event) {
+    function isNumber(x){
+      if( typeof(x) !== 'number' && typeof(x) !== 'string' )
+        return false;
+      else
+        return (x == parseFloat(x) && isFinite(x));
+    }
+    let app = this;
+    let newEventId = event.target.value;
+    if (!isNumber(newEventId)) {
+      return;
+    }
+    var newItems = [];
+    let getUrl = `http://localhost:3001/events/${newEventId}`;
+    axios.get(getUrl)
+         .then(function (response) {
+           if (response.status === 200) {
+             // Success
+             try {
+               // try parsing response JSON
+               newItems = response.data.event_items;
+               app.setState({
+                 event_id: newEventId,
+                 items: newItems,
+               });
+             } catch (e) {
+               // parse error
+             }
+           } else {
+             // Server rejected or server error
+           }})
+         .catch(function (error) {
+           // Not reach to Server
+           console.log(error);
+         });
+  }
+  
   render() {
     return (
       <div className="App">
@@ -29,8 +65,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="App-input">
+          Event_id: <input type="number" value={this.state.value} onChange={this.handleEventIdChange} />
+        </div>
         <EventItemsList items={this.state.items} />
-        <CheckoutButton />
+        {/* <CheckoutButton /> */}
       </div>
     );
   }
