ee66454340543a9a6bbe9ae710acbad89b84196e
urushiyama <aswif10flis1ntkb@gmail.com>
2017-11-28 23:29:18 +0900

    環境変数NODE_ENVを参照して環境ごとにconst-valuesの値を設定するように修正

--- src/const/const-values.js
+++ src/const/const-values.js
@@ -1,7 +1,33 @@
+function switchByEnv(values) {
+  let defaultValue = undefined;
+  if (values.default) {
+    defaultValue = values.default;
+  }
+  if (process.env.NODE_ENV === "development") {
+    return values.development || defaultValue;
+  }
+  switch (process.env.NODE_ENV) {
+    case "development":
+      return values.development || defaultValue;
+      break;
+    case "test":
+      return values.test || defaultValue;
+      break;
+    case "production":
+      return values.production || defaultValue;
+      break;
+    default:
+      return defaultValue;
+  }
+}
+console.log(process.env.NODE_ENV);
 export const BACK = `戻る`;
 export const CHECKOUT = `お会計`;
 export const DEPOSIT = `お預り金`;
-export const EVENTS_URI = `/events/`;
+export const EVENTS_URI = switchByEnv({
+  default: `http://localhost:3001/events/`,
+  production: `/events`,
+});
 export const MINUS = `-`;
 export const PAYMENT = `お支払`;
 export const PLUS = `+`;
