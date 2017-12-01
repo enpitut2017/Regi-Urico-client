1484a2b62d49ef92438bb1c740b6b459e547327d
urushiyama <aswif10flis1ntkb@gmail.com>
2017-12-01 16:31:27 +0900

    Compose handleChange... functions

--- src/CreateAccountForm.js
+++ src/CreateAccountForm.js
@@ -32,42 +32,37 @@ class CreateAccountForm extends Component {
       password: '',
       confirmPassword: '',
     }
-    this.handleChangeName = this.handleChangeName.bind(this);
-    this.handleChangePassword = this.handleChangePassword.bind(this);
-    this.handleChangeConfirmPassword = this.handleChangeConfirmPassword.bind(this);
-    this.disableCreateAccount = this.disableCreateAccount.bind(this);
   }
-  
-  handleChangeName(event) {
-    const newName = event.target.value;
-    this.setState({
-      name: newName,
-    });
+
+  handleChange = event => {
+    const newValue = event.target.value;
+    const id = event.target.id;
+    let newState = {};
+    switch (id) {
+      case "name":
+        newState.name = newValue;
+        break;
+      case "password":
+        newState.password = newValue;
+        break;
+      case "confirm-password":
+        newState.confirmPassword = newValue;
+        break;
+      default:
+        return;
+    }
+    this.setState(newState);
   }
-  
-  handleChangePassword(event) {
-    const newPassword = event.target.value;
-    this.setState({
-      password: newPassword,
-    });
-  }
-  
-  handleChangeConfirmPassword(event, newValue) {
-    const newConfirmPassword = event.target.value;
-    this.setState({
-      confirmPassword: newConfirmPassword,
-    });
-  }
-  
-  disableCreateAccount() {
+
+  disableCreateAccount = () => {
     return (
       this.state.name === ""
       || this.state.password === ""
       || this.state.password !== this.state.confirmPassword
     );
   }
-  
-  render() {
+
+  render = () => {
     return (
       <Grid container spacing={24} justify="center">
         <Grid item xs={10} md={6}>
@@ -83,7 +78,7 @@ class CreateAccountForm extends Component {
                     margin="normal"
                     fullWidth
                     value={this.state.name}
-                    onChange={this.handleChangeName}
+                    onChange={this.handleChange}
                   />
                 </Grid>
               </Grid>
@@ -98,7 +93,7 @@ class CreateAccountForm extends Component {
                     margin="normal"
                     fullWidth
                     value={this.state.password}
-                    onChange={this.handleChangePassword}
+                    onChange={this.handleChange}
                   />
                 </Grid>
               </Grid>
@@ -113,7 +108,7 @@ class CreateAccountForm extends Component {
                     margin="normal"
                     fullWidth
                     value={this.state.confirmPassword}
-                    onChange={this.handleChangeConfirmPassword}
+                    onChange={this.handleChange}
                   />
                 </Grid>
               </Grid>
