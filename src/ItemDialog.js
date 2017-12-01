ce27a7fb82379a6f714976ef5f17aca3c22b8a0b
Hasegawa Yohei <yohey@hasegawayouheinoMacBook-Air.local>
2017-12-01 21:23:11 +0900

    added: item dialog

---
+++ src/ItemDialog.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Dialog, { DialogTitle, DialogContent, DialogActions } from 'material-ui/Dialog';
+import Button from 'material-ui/Button';
+import { TextField } from 'material-ui';
+import { ITEM_NAME, ITEM_PRICE, ITEM_COUNT, CANCEL, SAVE } from './const/const-values';
+
+const styles = {
+  center: {
+    textAlign: 'center'
+  },
+  centerBlock: {
+    margin: '0 auto'
+  },
+  marginTop30: {
+    marginTop: 30
+  }
+};
+
+class ItemDialog extends Component {
+  render() {
+    return (
+      <Dialog onRequestClose={this.props.onRequestClose} open={this.props.open}>
+        <DialogTitle style={styles.center}></DialogTitle>
+        <DialogContent>
+          <TextField
+            name='name'
+            label={ITEM_NAME}
+            value={this.props.item.name}
+            onChange={this.props.handleChange}
+            type="text"
+            fullWidth
+           />
+          <TextField
+            name='price'
+            label={ITEM_PRICE}
+            value={this.props.item.price}
+            onChange={this.props.handleChange}
+            type="number"
+            style={styles.marginTop30}
+            fullWidth
+           />
+           <TextField
+            name='count'
+            label={ITEM_COUNT}
+            value={this.props.item.count}
+            onChange={this.props.handleChange}
+            type="number"
+            style={styles.marginTop30}
+            fullWidth
+           />
+        </DialogContent>
+        <DialogActions>
+          <Button color="accent" onClick={this.props.onRequestClose}>{CANCEL}</Button>
+          <Button disabled color="primary" onClick={this.props.onClickSave}>{SAVE}</Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default ItemDialog;
