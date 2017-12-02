6be4a98d4794b22a76b553b6dbfc442bd6d1e267
urushiyama <aswif10flis1ntkb@gmail.com>
2017-12-03 00:46:52 +0900

    画面上部のナビゲーションバーの描画コンポーネントを作成

---
+++ src/NavigationAppBar.js
@@ -0,0 +1,39 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from 'material-ui';
+import MenuIcon from 'material-ui-icons/Menu';
+import React from 'react';
+
+import { LOGOUT } from './const/const-values';
+
+const styles = {
+  title: {
+    flex: 1,
+  }
+}
+
+const NavigationAppBar = props => {
+  if (props.authorized) {
+    return (
+      <header>
+        <AppBar position="fixed">
+          <Toolbar>
+            <IconButton
+              color="contrast"
+              aria-label="Pick a Event"
+              onClick={props.handleOpenDrawer}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography type="title" color="inherit" style={styles.title}>
+              {props.title}
+            </Typography>
+            <Button color="contrast" onClick={props.handleSignOut}>
+              {LOGOUT}
+            </Button>
+          </Toolbar>
+        </AppBar>
+      </header>
+    );
+  }
+};
+
+export default NavigationAppBar;
