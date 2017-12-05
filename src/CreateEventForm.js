71d29341ac759052e2f4bb367ec0ce27e6458266
Hasegawa Yohei <yohey@hasegawayouheinoMacBook-Air.local>
2017-12-05 19:29:26 +0900

    modified: add actions

--- src/CreateEventForm.js
+++ src/CreateEventForm.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import Grid from 'material-ui/Grid';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
 import Typography from 'material-ui/Typography';
 import { NullOrEmpty } from './worker-service/formService';
-import { CREATE_EVENT, EVENT_NAME } from './const/const-values';
+import { XHRInstance } from './worker-service/axiosService';
+import { CREATE_EVENT, EVENT_NAME, BACK, EVENTS_URI } from './const/const-values';
+import { BASE_URI } from './const/urls';
 
 const styles = {
   gridPaper: {
@@ -22,43 +25,76 @@ const styles = {
   }
 };
 
-const CreateEventForm = props => {
-  return (
-    <Grid container spacing={24} justify='center'>
-      <Grid item xs={12} md={12} style={styles.serviceName}>
-        <Typography type='display1' gutterBottom align='center' color='secondary'>
-          {CREATE_EVENT}
-        </Typography>
-      </Grid>
-      <Grid item xs={10} md={6} style={styles.gridPaper}>
-        <Paper style={styles.paper}>
-          <Grid container spacing={24} justify='center'>
-            <Grid item xs={8} sm={4} md={4}>
-              <TextField
-                id='eventName'
-                name='eventName'
-                label={EVENT_NAME}
-                className='TextField'
-                margin='normal'
-                fullWidth
-              />
-            </Grid>
-          </Grid>
-          <Grid container spacing={24} justify='center'>
-            <Grid item xs={8} sm={4} md={4}>
-              <Button
-                id='createEvnet'
-                raised
-                color='primary'
-              >
-                {CREATE_EVENT}
-              </Button>
-            </Grid>
-          </Grid>
-        </Paper>
-      </Grid>
-    </Grid>
-  );
-}
+export default class CreateEventForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventName: '',
+      redirect: false
+    };
+  }
 
-export default CreateEventForm;
+  handleChange = e => {
+    const eventName = e.target.value;
+    this.setState({
+      eventName
+    });
+  }
+
+  handleClick = async () => {
+    const url = `${BASE_URI}${EVENTS_URI}`;
+    const eventName = this.state.eventName;
+    const response = await XHRInstance.post(url, {name: eventName});
+    localStorage.event_id = response.data.id;
+    this.setState({
+      redirect: true
+    });
+  }
+
+  renderCreateEventForm = () => {
+    return (
+      <Grid container spacing={24} justify='center'>
+        <Grid item xs={12} md={12} style={styles.serviceName}>
+          <Typography type='display1' gutterBottom align='center' color='secondary'>
+            {CREATE_EVENT}
+          </Typography>
+        </Grid>
+        <Grid item xs={10} md={6} style={styles.gridPaper}>
+          <Paper style={styles.paper}>
+            <Grid container spacing={24} justify='center'>
+              <Grid item xs={8} sm={4} md={4}>
+                <TextField
+                  id='eventName'
+                  name='eventName'
+                  label={EVENT_NAME}
+                  value={this.state.eventName}
+                  onChange={this.handleChange}
+                  className='TextField'
+                  margin='normal'
+                  fullWidth
+                />
+              </Grid>
+            </Grid>
+            <Grid container spacing={24} justify='center'>
+              <Grid item xs={8} sm={4} md={4}>
+                <Button
+                  id='createEvnet'
+                  raised
+                  color='primary'
+                  onClick={this.handleClick}
+                >
+                  {CREATE_EVENT}
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+    );
+  }
+
+  render() {
+    if (this.state.redirect) return <Redirect to='/'/>
+    return this.renderCreateEventForm();
+  }
+}
