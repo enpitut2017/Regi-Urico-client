8ef0572e7e4b9dfb21de22521f9eacf5e6a647c0
urushiyama <aswif10flis1ntkb@gmail.com>
2017-12-18 23:30:25 +0900

    locationオブジェクトを文字列と比較している為にリダイレクトループが解消されていない問題を修正

--- src/RedirectOnce.js
+++ src/RedirectOnce.js
@@ -11,7 +11,7 @@ import React from 'react';
  * @provide bool   props.if リダイレクトするならばtrue, しないならばfalse
  */
 const RedirectOnce = props => {
-  if (props.to === props.location || !props.if) {
+  if (props.to === props.location.pathname || !props.if) {
     return <div />;
   } else {
     return <Redirect to={props.to} />;
